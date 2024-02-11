3bb2811651bfc8ccbd70d8929c760110adf6dabe
dvlpr2003 <gayathrigaya698@gmail.com>
2024-02-10 23:56:10 -0800

    new one

---
+++ birthdaywish.py
@@ -0,0 +1,36 @@
+import datetime
+import pandas
+from smtplib import SMTP
+class BirthdayWish :
+    def __init__(self):
+        self.current_Date  = datetime.date.today()
+        self.splitWord = str(self.current_Date).split("-")
+        self.splitWord.reverse()
+        self.Dob = f"{self.splitWord[0]}-{self.splitWord[1]}-{self.splitWord[2]}"
+        self.readFile = pandas.read_csv("./BirthdayList.csv")
+        #check DOB to the current DATE
+        self.checkDOB = self.readFile[self.readFile["Date of Birth"]==self.Dob]
+        self.finalDOB=self.checkDOB.head()
+
+
+        self.Name =self.finalDOB["Name"]
+        self.Email = self.finalDOB["Mail"]
+        self.Send_wishes()
+        
+    def Send_wishes(self):
+        Hour = datetime.datetime.now().hour
+        Minute = datetime.datetime.now().minute
+        Second = datetime.datetime.now().second
+        print(Minute)
+
+        for _ in range(len(self.Name)):
+                
+    
+                
+                if (Hour == 23) and (Minute == 53) and (Second == 0):
+
+                    with SMTP("smtp.gmail.com") as connection:
+                        connection.starttls()
+                        connection.login(user="gayathrigaya698@gmail.com",password="lcnm qida weeq nxxn")
+                        connection.sendmail(from_addr="gayathrigaya698@gmail.com",to_addrs=f"{self.Email[_]}",msg=f"Subject:{'Birthday Wishes'}\n\nHappy birthday {self.Name[_]}\n fuck you da")
+                                    
--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-from datetime import datetime,date
-from smtplib import SMTP
-with open ("./birthday_list.txt","r") as file:
-    Birthday = file.readlines()
-
-name = []
-date_of_birth = []
-mail = []
-for _ in Birthday:
-    spl1 = _.split(",")
-    name.append(spl1[0])
-    Extract_dob = spl1[1].split("-")
-    date_of_birth.append(Extract_dob)
-    mail.append(spl1[2])
+from birthdaywish import BirthdayWish
+def main():
+    while True:
+        obj = BirthdayWish()
 
 
-condition = False 
-while not condition:
-    for _ in range(len(date_of_birth)):
-        year = date_of_birth[_][0]
-        month = date_of_birth[_][1]
-        day = date_of_birth[_][2]
-        Date = date(year=int(year),day=int(day),month=int(month))
-        currentDt = datetime.today()
-        if currentDt.date() == Date:
-            with SMTP("smtp.gmail.com") as connection:
-                connection.starttls()
-                connection.login(user="gayathrigaya698@gmail.com",password="lcnm qida weeq nxxn")
-                connection.sendmail(from_addr="gayathrigaya698@gmail.com",to_addrs=f"{mail[_]}",msg=f"Subject:{'Birthday Wishes'}\n\nHappy birthday {name[_]}")
-            condition = True
-        else:
-            continue
-            
-            
 
+if __name__ == "__main__":
+    main()
 
